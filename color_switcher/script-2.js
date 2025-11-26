912554c75128babb943055ba69844a044a317d21
GovindJindal <govindjindal7808@gmail.com>
2025-11-26 14:45:17 +0530

    New Changes Made

---
+++ color_switcher/script-2.js
@@ -0,0 +1,19 @@
+function changeColor(color){
+    document.body.style.backgroundColor= color;
+}
+
+document.getElementById("a").addEventListener("click", function() {
+    changeColor('red');
+})
+document.getElementById("b").addEventListener("click", function() {
+    changeColor('yellow');
+})
+document.getElementById("c").addEventListener("click", function() {
+    changeColor('green');
+})
+document.getElementById("d").addEventListener("click", function() {
+    changeColor('voilet');
+})
+document.getElementById("e").addEventListener("click", function() {
+    changeColor('magenta');
+})
