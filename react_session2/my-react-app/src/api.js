a61be730331dbdf3f543e75704ae356d533f002c
GovindJindal <govindjindal7808@gmail.com>
2025-12-02 01:38:10 +0530

    Added session2

---
+++ react_session2/my-react-app/src/api.js
@@ -0,0 +1,23 @@
+const API_Key = "4377284227a3717fbedfa5ca18b3a692";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export async function searchMovies(query){
+
+    const res = await fetch(`${BASE_URL}/search/movie?api_key=${API_Key}&query=${query}`);
+
+    const data = await res.json();
+    return data.results;
+
+}
+
+
+export async function getTopMovies(){
+
+
+     const res = await fetch(`${BASE_URL}/movie/popular?api_key=${API_Key}`);
+
+    
+    const data = await res.json();
+    return data.results;
+  
+}
